2c2604244781e100d42d476b1e14109d505eff6c
AbrorilHuda <moh.abrorilhuda@gmail.com>
2025-01-14 09:58:47 +0800

    generate: by dc assisten sample

---
+++ src/routes/Blog.ts
@@ -0,0 +1,17 @@
+// di generate oleh dc assistents
+import {Hono} from "hono";
+
+import {getBlogs, createBlog, getBlogById, updateBlogById, deleteBlogById} from "../controllers/blogController";
+
+
+const router = new Hono();
+/*
+@yourfunction itu function ubah function yang kalian buat
+atau menggunakan perintah bun dc make:route --controller
+*/
+
+router.get("/", (c) => getBlogs(c));
+router.post("/", (c) => createBlog(c));
+router.get("/:id", (c) => getBlogById(c));
+router.patch("/:id", (c) => updateBlogById(c));
+router.delete("/:id", (c) => deleteBlogById(c));
---
+++ types/User.ts
@@ -0,0 +1,4 @@
+// di generate oleh dc assistents
+export type UserType = {
+    // typeCode
+}
